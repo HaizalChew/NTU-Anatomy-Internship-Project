94af9a31560780dee98f0d0efcb4d210eaa3cfa5
hao199 <S10227951@connect.np.edu.sg>
2023-10-10 16:25:26 +0800

    Topic and Year Filter

--- Assets/Scripts/ContentFilter.cs
+++ Assets/Scripts/ContentFilter.cs
@@ -4,19 +4,50 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.WSA;
+using UnityEngine.Windows.Speech;
 
 public class ContentFilter : MonoBehaviour
 {
 
     public GameObject containerPanel;
     public TMP_Dropdown dropDownPanel;
+    public Dictionary<GameObject, GameObject> TopicContent = new Dictionary<GameObject, GameObject>();
+
+    public GameObject[] contentArray; 
+    public Button[] topicArray;
+
+    private bool ContentStatus;
+    private bool active;
+
+    public int him;
 
     // Start is called before the first frame update
     void Start()
     {
         FilterContent("Year1");
-        dropDownPanel = GetComponent<TMP_Dropdown>();
         dropDownPanel.onValueChanged.AddListener(delegate { DropDownPanelValueChanged(dropDownPanel); });
+        for (int i = 0; i < contentArray.Length; i++)
+        {
+            int count = i;
+            topicArray[count].onClick.AddListener(delegate { him = count; });
+            topicArray[count].onClick.AddListener(delegate { ShowContent(); });
+        }
+
+        //topicArray[0].onClick.AddListener(() => him = 0);
+        //topicArray[0].onClick.AddListener(delegate { ShowContent(); });
+        //topicArray[1].onClick.AddListener(() => him = 1);
+        //topicArray[1].onClick.AddListener(delegate { ShowContent(); });
+        //topicArray[2].onClick.AddListener(() => him = 2);
+        //topicArray[2].onClick.AddListener(delegate { ShowContent(); });
+
+
+    }
+
+    private int GetNumber(int i)
+    {
+        him = i; 
+        return i;
     }
 
     public void DropDownPanelValueChanged(TMP_Dropdown change)
@@ -46,9 +77,34 @@ public class ContentFilter : MonoBehaviour
         }
     }
 
+    public void ShowContentOnButton()
+    {
+
+        //ShowContent(contentArray[0]);
+
+    }
+
+
+    public void ShowContent()
+    {
+        if (contentArray[him].activeSelf == true)
+        {
+            contentArray[him].SetActive(false);
+        }
+        else
+        {
+            foreach (GameObject content in contentArray)
+            {
+                content.SetActive(false);
+            }
+            contentArray[him].SetActive(true);
+        }
+        
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+       //Debug.Log(him);
     }
 }
