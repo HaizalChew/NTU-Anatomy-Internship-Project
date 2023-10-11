4835d011419d90b80841c5bb834bc6d31cd1369f
hao199 <S10227951@connect.np.edu.sg>
2023-10-11 14:49:43 +0800

    ProgressBar

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
 public class MenuManager : MonoBehaviour
 {
     [SerializeField] private GameObject fadeCanvas;
--- Assets/Scripts/SaveStateManager.cs
+++ Assets/Scripts/SaveStateManager.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SaveStateManager : MonoBehaviour
 {
 
     public static SaveStateManager instance;
-
+    public Image bar;
     public bool[] topicChecklistCompleted = new bool[5];
+
+    public int maximum;
+    public int current;
 
     private void Awake()
     {
@@ -23,11 +27,12 @@ public class SaveStateManager : MonoBehaviour
 
         LoadData();
         Debug.Log(topicChecklistCompleted[1]);
-            
+
         //for (int i = 0; i < topicChecklistCompleted.Length; i++)
         //{
         //    topicChecklistCompleted[i] = true;
         //}
+        GetCurrentFill();
 
     }
 
@@ -43,6 +48,19 @@ public class SaveStateManager : MonoBehaviour
         return true;
     }
 
+    public int NumComplete()
+    {
+        int i = 0;
+        foreach (bool topicCheck in topicChecklistCompleted)
+        {
+            if (topicCheck)
+            {
+                i++;
+            }
+        }
+        return i;
+    }
+
     public void LoadData()
     {
         for ( int i = 0; i < topicChecklistCompleted.Length; i++ )
@@ -53,5 +71,13 @@ public class SaveStateManager : MonoBehaviour
                 Debug.Log("Check");
             }
         }
+    }
+
+    private void GetCurrentFill()
+    {
+        current = NumComplete();
+        maximum = 6;
+        float fillAmount = (float)current / (float)maximum;
+        bar.fillAmount = fillAmount;
     }
 }
